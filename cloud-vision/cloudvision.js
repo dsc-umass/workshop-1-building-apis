8bfbf6ee5ac711c113c78c629eb1d874f4f14733
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-12-02 02:27:06 -0500

    Cloud Vision

--- cloud-vision/cloudvision.js
+++ cloud-vision/cloudvision.js
@@ -1,16 +1,29 @@
 
 async function detect() {
-    // Imports the Google Cloud client library
-    const vision = require('@google-cloud/vision');
+    
+    try {
+
+        const vision = require('@google-cloud/vision');
   
     // Creates a client
-    const client = new vision.ImageAnnotatorClient();
+    const client = new vision.ImageAnnotatorClient({
+        key: "./dsc-workshop.json"
+
+    });
   
     // Performs label detection on the image file
     const [result] = await client.labelDetection('./image.jpg');
     const labels = result.labelAnnotations;
     console.log('Labels:');
     labels.forEach(label => console.log(label.description));
+
+    }
+    catch(error) {
+
+        console.log(error)
+
+    }
+    
   }
 
   detect()
